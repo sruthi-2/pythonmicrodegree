81fcfc4c5f6214b4826415bf030d314e9ff4619d
sruthi-2 <71808593+sruthi-2@users.noreply.github.com>
2021-05-22 12:16:46 +0530

    dir function

---
+++ dirfunction.py
@@ -0,0 +1,11 @@
+#what all thinga you can do with a specific datatype
+'''
+dir() is a powerful inbuilt function in Python3, which returns list of the attributes and methods of any object (say functions , modules, strings, lists, dictionaries etc.)
+dir({object})
+'''
+import string
+
+print(dir)
+print(dir(int))
+print(dir(string))
+print(dir(float))
